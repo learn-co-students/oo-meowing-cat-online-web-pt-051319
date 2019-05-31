2557d50fac79acccf01d5a82d73c0f8401afcbfc
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-05-31 05:21:07 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
-## code your solution here. 
+class Cat
+  def name=(cat_name)
+    @name
+  end
+  def name
+  @name
+  def cat_meow
+  @cat_meow
+end
+def cat_meow=(cat_meow)
+  @cat_meow
+  end
+  
+end
+  
